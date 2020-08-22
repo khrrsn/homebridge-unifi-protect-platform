69df492c4dd45ff3d232b11d2ced6e569544994d
Shaun <mail@shnhrrsn.com>
2020-08-22 00:26:25 -0400

    feat: add doorbell as switch

--- src/accessories/doorbellAccessory.ts
+++ src/accessories/doorbellAccessory.ts
@@ -6,14 +6,27 @@ const typeAllowlist = new Set(['UVC G4 Doorbell'])
 
 export default <accessory<Camera>>function doorbellAccessory({ hap }, services, _device, stream) {
 	const { Characteristic, Service } = hap
+	const onRing = stream.pipe(filter(message => message.body?.type === 'ring'))
+
 	services.registerObservableCharacteristic({
 		characteristicType: Characteristic.ProgrammableSwitchEvent,
 		serviceType: Service.Doorbell,
-		onValue: stream.pipe(
-			filter(message => message.body?.type === 'ring'),
-			mapTo(Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS),
-		),
+		onValue: onRing.pipe(mapTo(Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS)),
 	})
+
+	services.registerObservableCharacteristic({
+		characteristicType: Characteristic.ProgrammableSwitchEvent,
+		serviceType: Service.StatelessProgrammableSwitch,
+		onValue: onRing.pipe(mapTo(Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS)),
+	})
+
+	// Hide long and double press events by setting max value
+	services
+		.getService(Service.StatelessProgrammableSwitch)
+		.getCharacteristic(Characteristic.ProgrammableSwitchEvent)
+		.setProps({
+			maxValue: Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS,
+		})
 }
 
 export function isDoorbell(camera: Camera) {
