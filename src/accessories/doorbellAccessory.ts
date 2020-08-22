d492fd197e92b2e81863f73aa72afab72fd5debd
Shaun <mail@shnhrrsn.com>
2020-08-22 00:20:34 -0400

    feat: initial doorbell support

--- src/accessories/doorbellAccessory.ts
+++ src/accessories/doorbellAccessory.ts
@@ -1,10 +1,19 @@
 import { Camera } from '../protect/api'
 import { accessory } from './accessory'
+import { filter, mapTo } from 'rxjs/operators'
 
 const typeAllowlist = new Set(['UVC G4 Doorbell'])
 
-export default <accessory<Camera>>function doorbellAccessory() {
-	// TODO
+export default <accessory<Camera>>function doorbellAccessory({ hap }, services, _device, stream) {
+	const { Characteristic, Service } = hap
+	services.registerObservableCharacteristic({
+		characteristicType: Characteristic.ProgrammableSwitchEvent,
+		serviceType: Service.Doorbell,
+		onValue: stream.pipe(
+			filter(message => message.body?.type === 'ring'),
+			mapTo(Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS),
+		),
+	})
 }
 
 export function isDoorbell(camera: Camera) {
